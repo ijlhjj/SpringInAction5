221d35f7bf24008b53b93649539ae55a1b5f4b77
温馨佳园 <ijlhjj@126.com>
2021-10-03 13:16:10 +0800

    添加Taco设计和订单处理流程。

---
+++ src/main/java/com/sweetmanor/springinaction/tacos/domain/Taco.java
@@ -0,0 +1,23 @@
+package com.sweetmanor.springinaction.tacos.domain;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+/**
+ * 墨西哥煎玉米卷
+ */
+@Data
+public class Taco {
+
+	@NotNull
+	@Size(min = 5, message = "Name must be at least 5 characters long")
+	private String name;
+
+	@Size(min = 1, message = "You must choose at least 1 ingredient")
+	private List<String> ingredients;
+
+}
